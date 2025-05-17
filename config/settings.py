450169ca91c48142c39b7154e306c4007f7981db
Mohammad Safarzadeh <72881511+mammadsafar@users.noreply.github.com>
2025-05-17 20:08:22 +0330

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -110,7 +110,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'db',
+        'HOST': 'test-storedb-hrloo4',
         'PORT': 5432
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
